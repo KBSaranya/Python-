8348c3746fb8061af9dc0a9561ff5be59728fa2c
Saranya <67630803+KBSaranya@users.noreply.github.com>
2021-09-14 13:28:53 +0530

    addded!!

--- hungry.py
+++ hungry.py
@@ -2,4 +2,4 @@ hungry=input("Are you hungry")
 if hungry=="yes" and hungry=="Y" and hungry=="y":
     print("eat something")
 else :
-    print("MY stomach is full")
+    print("MY stomach is full!!")
